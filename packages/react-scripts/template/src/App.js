a8cbf6bf6eff0859ef65515f1a51fc52172b2156
Wu Huijie <hjwu85@gmail.com>
2018-06-01 00:25:57 +0800

    add styled components

--- packages/react-scripts/template/src/App.js
+++ packages/react-scripts/template/src/App.js
@@ -1,19 +1,24 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
 import logo from './logo.svg';
 import './App.css';
+
+const Container = styled.div`text-align: center;`;
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <Container>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React created with Custom React Scripts!</h1>
+          <h1 className="App-title">
+            Welcome to React created with Custom React Scripts!
+          </h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-      </div>
+      </Container>
     );
   }
 }
