867b432c56f72c02fd26ed19751ae8626b99f5cf
Wu Huijie <hjwu85@gmail.com>
2018-06-01 00:35:21 +0800

    moved styles to be managed by styled components

--- packages/react-scripts/template/src/App.js
+++ packages/react-scripts/template/src/App.js
@@ -5,19 +5,28 @@ import './App.css';
 
 const Container = styled.div`text-align: center;`;
 
+const Header = styled.header`
+  background-color: #222;
+  height: 150px;
+  padding: 20px;
+  color: white;
+`;
+
+const Title = styled.h1`font-size: 1.5em;`;
+
+const Intro = styled.p`font-size: large;`;
+
 class App extends Component {
   render() {
     return (
       <Container>
-        <header className="App-header">
+        <Header>
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">
-            Welcome to React created with Custom React Scripts!
-          </h1>
-        </header>
-        <p className="App-intro">
+          <Title>Welcome to React created with Custom React Scripts!</Title>
+        </Header>
+        <Intro>
           To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        </Intro>
       </Container>
     );
   }
