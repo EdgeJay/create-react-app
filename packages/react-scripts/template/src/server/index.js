8536aeeee461b3fd9725fa2f4c3ea46ea6533efd
Wu Huijie <hjwu85@gmail.com>
2018-06-03 17:45:31 +0800

    initial server app setup

--- packages/react-scripts/template/src/server/index.js
+++ packages/react-scripts/template/src/server/index.js
@@ -1,1 +1,11 @@
-console.log('start server!');
+const dotenv = require('dotenv');
+const dotenvExpand = require('dotenv-expand');
+
+dotenvExpand(dotenv.config());
+
+require('babel-register')({
+  ignore: /\/(public|node_modules)\//,
+  presets: ['env', 'react-app'],
+});
+
+require('./server.js');
