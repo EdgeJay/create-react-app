178f57e296fbb0c100278900c1c75dbf627404d1
Wu Huijie <hjwu85@gmail.com>
2018-06-13 21:38:18 +0800

    add new methods to detect app modes

--- packages/react-scripts/template/src/common/utils/appUtil.js
+++ packages/react-scripts/template/src/common/utils/appUtil.js
@@ -1,3 +1,11 @@
+export function isInProductionMode() {
+  return process.env.NODE_ENV === 'production';
+}
+
+export function isInDevelopmentMode() {
+  return process.env.NODE_ENV === 'development';
+}
+
 export function isClient() {
   return typeof window === 'object' && window.document && window.document.createElement;
 }
