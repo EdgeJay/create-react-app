7dbf48cfc5f11e4d67d5f8ff585688282ae52710
Wu Huijie <hjwu85@gmail.com>
2018-06-14 00:53:40 +0800

    update babel settings used at server-side

--- packages/react-scripts/template/src/server/index.js
+++ packages/react-scripts/template/src/server/index.js
@@ -6,6 +6,28 @@ dotenvExpand(dotenv.config());
 require('babel-register')({
   ignore: /\/(public|node_modules)\//,
   presets: ['env', 'react-app'],
+  plugins: [
+    [
+      'transform-assets-import-to-string',
+      {
+        extensions: [
+          '.html',
+          '.css',
+          '.ico',
+          '.png',
+          '.jpg',
+          '.jpeg',
+          '.svg',
+          '.gif',
+          '.otf',
+          '.ttf',
+          '.webm',
+        ],
+        baseDir: '/',
+        baseUri: '',
+      },
+    ],
+  ],
 });
 
 require('./server.js');
