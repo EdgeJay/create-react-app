cabd1f502dc1f74a8def7e864a35eaf7da21a785
Wu Huijie <hjwu85@gmail.com>
2018-06-17 01:42:55 +0800

    do not call hydrate when container elem is empty

--- packages/react-scripts/template/src/index.js
+++ packages/react-scripts/template/src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render, hydrate } from 'react-dom';
 import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'connected-react-router';
 import './client/assets/css/index.css';
@@ -9,14 +9,16 @@ import createStore from './common/store';
 
 async function initialize() {
   const { store, history } = await createStore();
+  const container = document.getElementById('root');
+  const renderFunc = container.children.length > 0 ? hydrate : render;
 
-  ReactDOM.hydrate(
+  renderFunc(
     <Provider store={store}>
       <ConnectedRouter history={history}>
         <App />
       </ConnectedRouter>
     </Provider>,
-    document.getElementById('root')
+    container
   );
 
   registerServiceWorker();
